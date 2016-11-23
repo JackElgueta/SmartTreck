5e24499c5a9efff2a08a74999e8152d03aadc74a
JackElgueta <jack.elgueta@mail.udp.cl>
2016-11-23 16:20:58 -0300

    final

--- app/src/main/java/com/example/jack/smarttreck/EquipamientoActivity.java
+++ app/src/main/java/com/example/jack/smarttreck/EquipamientoActivity.java
@@ -2,12 +2,24 @@ package com.example.jack.smarttreck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class EquipamientoActivity extends AppCompatActivity {
+
+    Spinner zapatos;
+    String[] listZapatos= {"Zapatos", "Blandos", "Semirígidos", "Rígidos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipamiento);
+
+        zapatos= (Spinner)findViewById(R.id.Zapatos);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,listZapatos);
+        zapatos.setAdapter(adaptador);
     }
+
+
 }
