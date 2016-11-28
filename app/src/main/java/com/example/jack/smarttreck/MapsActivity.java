3fec4b5f90ec01c2e8cf2d98b7f11187a43e0739
marcelcaceres <mcaceres.bretti@gmail.com>
2016-11-28 00:24:56 -0300

    Icono + map activity recibiendo rutas

--- app/src/main/java/com/example/jack/smarttreck/MapsActivity.java
+++ app/src/main/java/com/example/jack/smarttreck/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.jack.smarttreck;
 
 import android.*;
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -11,6 +12,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.ListView;
 
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdate;
@@ -22,16 +25,30 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    String nombreApellido, nivel, token, sessid, session_name, fid_image, url_image, uid_user;
     private GoogleMap mMap;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null){
+            token = extras.getString("token");
+            sessid = extras.getString("sessid");
+            session_name = extras.getString("session_name");
+        }
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -43,7 +60,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        final ProgressDialog progressDialog = new ProgressDialog(MapsActivity.this);
+        progressDialog.setMessage("Cargando Equipamiento...");
+        progressDialog.show();
+
+        AsyncHttpClient client_map = new AsyncHttpClient();
+        client_map.addHeader("X-CSRF-Token", token);
+        client_map.addHeader("Cookie", session_name + "=" + sessid);
+
+        client_map.get("http://itfactory.cl/smartTrekking/api/views/rutas", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if(statusCode==200){
+                    Log.d("response", new String(responseBody));
+                    progressDialog.dismiss();
+                    try {
+                        JSONArray jsonArray = new JSONArray(new String(responseBody));
+                        for (int i=0;i<jsonArray.length();i++) {
+                            mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(Double.parseDouble(jsonArray.getJSONObject(i).getString("lat")), Double.parseDouble(jsonArray.getJSONObject(i).getString("lon"))))
+                            .title(jsonArray.getJSONObject(i).getString("node_title"))
+                            );
+
+                        }
+
+
+
+                    }catch (JSONException e){
+
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
